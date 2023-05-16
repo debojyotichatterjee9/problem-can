526ce6253b52e1914fd77c272b4b7dcac1ca9358
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2023-05-16 18:38:59 +0530

    js:3c3a5515-7d4b-4950-8036-4660be6ad28b

--- set_1/easy/javascript/3c3a5515-7d4b-4950-8036-4660be6ad28b.js
+++ set_1/easy/javascript/3c3a5515-7d4b-4950-8036-4660be6ad28b.js
@@ -1,12 +1,60 @@
-// This is the input:
-const input = [10, 4, 3, 50, 23, 90]
+
+const removeDupes = arr => {
+    let tempArr = [];
+    arr.forEach(element => {
+        if (!tempArr.includes(element)) {
+            tempArr.push(element)
+        }
+    });
+    return tempArr;
+}
 
 // Solution 1:
-const arr = input;
-const sortedArr = arr.sort((elem1, elem2) => {
-    return elem1 - elem2
-});
 
-console.log(sortedArr.splice(sortedArr.length - 3, 3))
+const printLargestThree1 = (arr) => {
+    const sortedArr = arr.sort((elem1, elem2) => {
+        return elem1 - elem2
+    });
+    return (sortedArr.splice(sortedArr.length - 3, 3));
+}
+const arr1 = [10, 4, 3, 50, 23, 90];
+const res1 = printLargestThree1(arr1);
+console.log(res1);
+
 
 // Solution 2:
+// Function to print three largest elements
+const printLargestThree2 = (arr, arr_size) => {
+    let first, second, third;
+    arr = removeDupes(arr);
+    // There should be atleast three elements
+    if (arr_size < 3) {
+        return ("Invalid Input!!!");
+    }
+
+    third = first = second = -1;
+    for (let i = 0; i < arr_size; i++) {
+        if (arr[i] > first) {
+            third = second;
+            second = first;
+            first = arr[i];
+        }
+
+        else if (arr[i] > second) {
+            third = second;
+            second = arr[i];
+        }
+
+        else if (arr[i] > third)
+            third = arr[i];
+    }
+
+    return ([first, second, third])
+}
+
+// Driver code
+let arr2 = [10, 4, 3, 50, 23, 90, 90, 90, 90, 90, 50, 90];
+let n = arr2.length;
+
+const res = printLargestThree2(arr2, n);
+console.log(res);
