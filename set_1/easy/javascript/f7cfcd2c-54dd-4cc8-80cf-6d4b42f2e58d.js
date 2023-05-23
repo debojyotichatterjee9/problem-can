1852198f79092a0e707e71a11499ee5b4a07384f
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2023-05-23 19:39:27 +0530

    js:f7cfcd2c-54dd-4cc8-80cf-6d4b42f2e58d

--- set_1/easy/javascript/f7cfcd2c-54dd-4cc8-80cf-6d4b42f2e58d.js
+++ set_1/easy/javascript/f7cfcd2c-54dd-4cc8-80cf-6d4b42f2e58d.js
@@ -27,4 +27,27 @@ const arr1 = [9, 10, 16, 20, 26, 29, 30, 35, 41, 43];
 console.log(`The input array --> [${arr1}]`)
 
 const res1 = evenOddRearranger(arr1);
-console.log(res1)
+console.log(`The array after even-odd re-arrangement --> [${res1}]`);
+
+
+// Solution 2:
+const evenOddRearranger2 = arr => {
+    let arrLength = arr.length;
+    let [i,j] = [-1,0];
+    while (j != arrLength) {
+        if (arr[j] % 2 == 0) {
+            i++;
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        j++;
+    }
+
+    return arr
+}
+
+const arr2 = [9, 18, 23, 28, 31, 32, 35, 38, 48, 49];
+console.log(`The input array --> [${arr2}]`)
+const res2 = evenOddRearranger2(arr2);
+console.log(`The array after even-odd re-arrangement --> [${res2}]`);
