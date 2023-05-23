18be7dd298b04f0ad7db9863a4cfcc5a5f7fbc36
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2023-05-23 19:38:29 +0530

    js:f7cfcd2c-54dd-4cc8-80cf-6d4b42f2e58d

--- set_1/easy/javascript/feb1f421-6645-4402-bc05-051d4dde2178.js
+++ set_1/easy/javascript/feb1f421-6645-4402-bc05-051d4dde2178.js
@@ -25,3 +25,26 @@ const n = 3
 console.log(`The input array --> [${arr1}]`);
 const res1 = findNLargest(arr1, n);
 console.log(`The ${n}th largest number is --> ${res1}`);
+
+
+// Solution 2:
+const evenOddRearranger2 = arr => {
+    let arrLength = arr.length;
+    let [i,j] = [-1,0];
+    while (j != arrLength) {
+        if (arr[j] % 2 == 0) {
+            i++;
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        j++;
+    }
+
+    return arr
+}
+
+const arr2 = [9, 18, 23, 28, 31, 32, 35, 38, 48, 49];
+console.log(`The input array --> [${arr2}]`)
+const res2 = evenOddRearranger2(arr2);
+console.log(`The array after even-odd re-arrangement --> [${res2}]`);
