061e4d27f429ebbbec0c8e57cdf54d7d3e8d162e
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2023-05-23 20:22:41 +0530

    python:f7cfcd2c-54dd-4cc8-80cf-6d4b42f2e58d

--- set_1/easy/python/f7cfcd2c-54dd-4cc8-80cf-6d4b42f2e58d.py
+++ set_1/easy/python/f7cfcd2c-54dd-4cc8-80cf-6d4b42f2e58d.py
@@ -20,3 +20,24 @@ arr1 = [8, 10, 11, 17, 29, 31, 32, 35, 38, 43]
 print(f"The input array --> {arr1}")
 res1 = even_odd_rearranger(arr1)
 print(f"The array after even-odd re-arrangement --> {res1}")
+
+
+# Solution 2:
+
+def even_odd_rearranger2(arr):
+    arr_len = len(arr)
+    i=-1
+    j=0
+    while j != arr_len:
+        if arr[j] % 2 == 0:
+            i += 1
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+        j += 1
+    return arr
+    
+arr2 = [8, 10, 23, 25, 28, 29, 31, 36, 44, 48]
+print(f"The input array --> {arr2}")
+res2 = even_odd_rearranger2(arr2)
+print(f"The array after even-odd re-arrangement --> {res2}")
