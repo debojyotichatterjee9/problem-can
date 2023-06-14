9170bea35856848a2f59fd14283ff1ede668742e
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2023-06-14 23:47:14 +0530

    js:7ded2b05-0eee-4b97-b978-4cdf8d9bf695

--- set_1/easy/javascript/7ded2b05-0eee-4b97-b978-4cdf8d9bf695.js
+++ set_1/easy/javascript/7ded2b05-0eee-4b97-b978-4cdf8d9bf695.js
@@ -4,10 +4,10 @@ const arraySorter = arr => {
     return arr.sort((elem1, elem2) => elem1 - elem2);
 }
 
-const elementSwapper = (arr, index) => {
-    let tempElem = arr[index];
-    arr[index] = arr[index + 1];
-    arr[index + 1] = tempElem
+const elementSwapper = (arr, index1, index2) => {
+    let tempElem = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = tempElem
     return arr;
 }
 
@@ -15,12 +15,34 @@ const generateWaveArray = arr => {
     let [sortedArray, waveArray] = [arraySorter(arr), []];
     const arrayLength = sortedArray.length;
     for (let i = 0; i < arrayLength; i += 2) {
-        waveArray = elementSwapper(sortedArray, i);
+        elementSwapper(sortedArray, i, i + 1);
     }
-    return waveArray;
+    return sortedArray;
 }
 
-const arr1 = [23, 39, 44, 49, 53, 58, 66, 73, 76, 86];
+const arr1 = [87, 81, 20, 73, 79, 39, 30, 89, 35, 34];
 console.log(`The original input1 --> [${arr1}]`);
 const resp1 = generateWaveArray(arr1);
 console.log(`The wave array generated --> [${resp1}]`);
+
+
+// Solution 2:
+
+const generateWaveArray2 = arr => {
+    const arrLength = arr.length;
+    for (let i = 0; i < arrLength; i += 2) {
+        if (i > 0 && arr[i - 1] > arr[i]) {
+            elementSwapper(arr, i - 1, i);
+        }
+        if (i < arrLength - 1 && arr[i] < arr[i + 1]) {
+            elementSwapper(arr, i, i + 1);
+        }
+    }
+    return arr;
+}
+
+const arr2 = [48, 44, 16, 64, 33, 91, 84, 38, 74, 54];
+console.log(`The original input1 --> [${arr2}]`);
+const resp2 = generateWaveArray2(arr2);
+console.log(`The wave array generated --> [${resp2}]`);
+
