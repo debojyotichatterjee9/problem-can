5e0f57fe616d9f291e58cc02f95670d133c8d3e7
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2023-06-30 23:47:05 +0530

    js:d03b9402-989a-4934-9b9f-c8f2bb839f4e

--- set_1/arrays/javascript/d03b9402-989a-4934-9b9f-c8f2bb839f4e.js
+++ set_1/arrays/javascript/d03b9402-989a-4934-9b9f-c8f2bb839f4e.js
@@ -26,3 +26,33 @@ let arr1 = [96, 87, 15, 73, 16, 78, 35, 81, 45, 37];
 resp1 = findNumberOfTriangles(arr1);
 console.log(`The number of possible traingles are --> ${resp1.count}`);
 console.log("The possible valid traingles are -->", resp1.resultantArr);
+
+// Solution 2:
+// TODO: This solution is not working as expected
+const arraySorter = arr => arr.sort((elem1, elem2) => elem1 - elem2)
+
+const findNumberOfTriangles2 = arr => {
+    let arrayLength = arr.length;
+    // Sort the array elements in non-decreasing order
+    let sortedArray = arraySorter(arr);
+    console.log(sortedArray);
+
+    // Initialize count of triangles
+    let count = 0;
+
+    for (let i = 0; i < arrayLength - 2; i++) {
+        let k = i + 2;
+        for (let j = i + 1; j < arrayLength; j++) {
+            while (k < arrayLength && sortedArray[i] + sortedArray[j] > sortedArray[k])
+                k++;
+
+            if (k > j)
+                count += k - j - 1;
+        }
+    }
+    return count;
+}
+
+const arr2 = [96, 87, 15, 73, 16, 78, 35, 81, 45, 37];
+const resp2 = findNumberOfTriangles2(arr2);
+console.log(`The number of possible triangles --> ${resp2}`);
