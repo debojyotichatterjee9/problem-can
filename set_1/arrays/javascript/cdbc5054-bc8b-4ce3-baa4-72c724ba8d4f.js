ec1a0265cc024ace6e03ea86f79c38ced80447f8
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2024-02-20 22:28:44 +0530

    js:cdbc5054-bc8b-4ce3-baa4-72c724ba8d4f

--- set_1/arrays/javascript/cdbc5054-bc8b-4ce3-baa4-72c724ba8d4f.js
+++ set_1/arrays/javascript/cdbc5054-bc8b-4ce3-baa4-72c724ba8d4f.js
@@ -1,5 +1,9 @@
 const arr1 = [1, 4, 20, 3, 10, 5];
 const SUM1 = 33;
+const arr2 = [15, 2, 4, 8, 5, 10, 23];
+const SUM2 = 21;
+
+// Solution 1:
 
 const findSubArrayWithSum = (arr, sum) => {
     const arrayLen = arr.length;
@@ -25,3 +29,32 @@ const findSubArrayWithSum = (arr, sum) => {
 }
 const resp1 = findSubArrayWithSum(arr1, SUM1);
 console.log(`The sub-array that sums to ${SUM1} is --> ${resp1}`);
+
+
+
+// Solution 2:
+
+const findSubArrayWithSum2 = (arr, sum) => {
+    const arrLen = arr.length;
+    const map = new Map();
+    let currSum = 0;
+    for (let i = 0; i < arrLen; i++) {
+        currSum += arr[i];
+        if (currSum === sum) {
+            return [0, i];
+        }
+        if (map.has(currSum - sum)) {
+            return [map.get(currSum - sum) + 1, i];
+        }
+        map.set(currSum, i);
+    }
+    return [];
+}
+
+
+
+const resp2 = findSubArrayWithSum2(arr2, SUM2);
+console.log(resp2)
+resp2.length === 0 ? 
+console.log(`No subarray with sum ${SUM2} found.`) : 
+console.log(`Sum found between indexes --> ${resp2[0]} and ${resp2[1]}`);
