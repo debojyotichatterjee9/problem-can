c4e157066299f0173cc859f07de9743e12848948
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2024-02-21 22:27:40 +0530

    js:a38a1b36-5149-4da2-a4be-858c46ad375b

--- set_1/arrays/javascript/a38a1b36-5149-4da2-a4be-858c46ad375b.js
+++ set_1/arrays/javascript/a38a1b36-5149-4da2-a4be-858c46ad375b.js
@@ -1,34 +1,44 @@
-const arr1 = [-1, -1, 6, 1, 9, 3, 2, -1, 4, -1]
+const arr1 = [-1, -1, 6, 1, 9, 3, 2, -1, 4, -1];
 
 // Output : [-1, 1, 2, 3, 4, -1, 6, -1, -1, 9]
 
+// Solution 1:
+
+const arraySortOnIndex = (arr1) => {
+    return arr1.map((elem, index) => {
+        if(elem !== index) {
+            const elemToReplace = arr1.find(elem => elem === index) || -1;
+            return elemToReplace;
+        }
+    });
+}
+
+const resp1 = arraySortOnIndex(arr1);
+console.log(`Re-arranged array --> ${resp1}`);
+
+// Solution 2:
+
 function fixArray(arr1) {
     const arrayLength = arr1.length;
-    var i, j, temp;
-
-    // Iterate over the array
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            // Check is any ar[j]
-            // exists such that
-            // ar[j] is equal to i
-            if (ar[j] == i) {
-                temp = ar[j];
-                ar[j] = ar[i];
-                ar[i] = temp;
+    let i, j, temp;
+    for (i = 0; i < arrayLength; i++) {
+        for (j = 0; j < arrayLength; j++) {
+            if (arr1[j] == i) {
+                temp = arr1[j];
+                arr1[j] = arr1[i];
+                arr1[i] = temp;
                 break;
             }
         }
     }
-
-    // Iterate over array
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < arrayLength; i++) {
         // If not present
-        if (ar[i] != i) {
-            ar[i] = -1;
+        if (arr1[i] != i) {
+            arr1[i] = -1;
         }
     }
-    return ar;
+    return arr1;
 }
 
-const resp = fixArray(arr1);
+const resp2 = fixArray(arr1);
+console.log(`Re-arranged array --> ${resp2}`);
