74ecbfeca69f348fdbf5e91108e8cb1126ef00e8
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2024-02-23 20:25:08 +0530

    py:a38a1b36-5149-4da2-a4be-858c46ad375b

--- set_1/arrays/python/a38a1b36-5149-4da2-a4be-858c46ad375b.py
+++ set_1/arrays/python/a38a1b36-5149-4da2-a4be-858c46ad375b.py
@@ -1,4 +1,7 @@
-arr1 = [ -1, -1, 6, 1, 9, 3, 2, -1, 4, -1 ]
+arr1 = [-1, -1, 6, 1, 9, 3, 2, -1, 4, -1]
+
+# Solution 1:
+
 
 def array_sort_on_index_1(arr1):
     array_length = len(arr1)
@@ -11,6 +14,27 @@ def array_sort_on_index_1(arr1):
             arr1[i] = -1
     return arr1
 
-resp1 = array_sort_on_index_1(arr1);
+
+resp1 = array_sort_on_index_1(arr1)
 
 print(f'The sorted array based on index --> {resp1}')
+
+
+# Solution 2:
+
+def array_sort_on_index_2(arr):
+    array_set = set()
+    array_length = len(arr)
+
+    for i in range(array_length):
+        array_set.add(arr[i])
+    for i in range(array_length):
+        if i in array_set:
+            arr[i] = i
+        else:
+            arr[i] = -1
+    return arr
+
+
+resp2 = array_sort_on_index_1(arr1)
+print(f'The sorted array based on index --> {resp2}')
