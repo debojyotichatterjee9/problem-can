f313567f3c51937b1440d5383c7e9cc36b1b93ac
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2026-03-30 22:38:45 +0530

    feat: 35 python soln

--- leet_code/problems/easy/python/035.py
+++ leet_code/problems/easy/python/035.py
@@ -16,17 +16,36 @@ Input: nums = [1,3,5,6], target = 7
 Output: 4
 """
 
+from typing import List
+import bisect
+
+
 class Solution:
-    def findPos1(self, nums: list[int], target: int) -> int:
-    	for i in range(len(nums)):
-    		if nums[i] == target or nums[i] > target:
-    			return i
-    		if nums[i] < target and nums[i+1] > target:
-    			return i+1
-    		if nums[i] == nums[len(nums)-1]:
-    			return i+1
-    			
-nums = [1,3,5,6]
+    def findPos1(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+            if i + 1 < len(nums) and nums[i] < target < nums[i + 1]:
+                return i + 1
+        return len(nums)
+
+    def findPos2(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
+
+    def findPos3(self, nums: List[int], target: int) -> int:
+        return bisect.bisect_left(nums, target)
+
+
+nums = [1, 3, 5, 6]
 target = 4
 solObj = Solution()
 resp = solObj.findPos1(nums, target)
